a90261cf6e9a5bbea481f7e4859e22c4881aff08
victor-butita <150669322+victor-butita@users.noreply.github.com>
2024-01-24 19:29:23 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Todo from "./Components/Todo";
+import TodoItems from "./Components/TodoItems";
+
+const App = () => {
+
+ 
+
+  return(
+    <div>
+    <Todo />
+    <TodoItems />
+  
+    </div>
+  )
+  
+}
+
+export default App;
